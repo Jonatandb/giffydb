f16d8c8f3de5f882b84c797d35421eb0bad75004
Jonatandb <jonatandb@gmail.com>
2020-09-07 23:03:47 -0300

    Agregado de indicador "Cargando..."

--- src/components/ListOfGifs/index.jsx
+++ src/components/ListOfGifs/index.jsx
@@ -4,14 +4,20 @@ import getGifs from '../../services/getGifs'
 
 export default function ListOfGifs({ params }) {
   const [gifs, setGifs] = useState([])
+  const [loading, setLoading] = useState(false)
 
   const { keyword } = params
 
   useEffect(() => {
+    setLoading(true)
     getGifs({ keyword }).then(gifs => {
       setGifs(gifs)
+      setLoading(false)
     })
   }, [keyword])
+
+  if (loading) return <i>Cargando...</i>
+
   return (
     <>
       {gifs.map(({ id, url, title }) => (
